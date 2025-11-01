cfff405c83fc16705111cb43e1a8ffbc2b653500
EduardoGZ1 <ddzk1111@gmail.com>
2025-10-31 22:38:01 -0300

    v0.2

---
+++ conectar.php
@@ -0,0 +1,8 @@
+<?php
+/* Dados do Banco de Dados a conectar */
+$Servidor = 'sql212.infinityfree.com';
+$nomeBanco = 'if0_40297372_teste';
+$Usuario = 'if0_40297372';
+$Senha = 'g5PcYqsvM5YB';
+$strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco);
+?>
